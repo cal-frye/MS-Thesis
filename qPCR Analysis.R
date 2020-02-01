aa1ac016a93ca2c0bcd67d9752e14cee89d8b37c
Cal <58495896+cal-frye@users.noreply.github.com>
2020-02-01 18:46:51 -0500

    Add files via upload

---
+++ qPCR Analysis.R
@@ -0,0 +1,17 @@
+#Analysis of qPCR data
+
+#Load packages and data
+library(ggplot2)
+library(stringr)
+
+data <- read.csv("NAME", header = T)
+
+
+
+
+#Need to load raw data
+#Clean the data? Investigate what samples failed to replicate
+#Indicate gene towhich all will be normalized
+#Do concentration calcuations w/ normalizing gene
+#Stastically analyse the data
+#plot the data
