1d7e59292b4632a8d946118c0bd8f5957f9cad9e
Cal <58495896+cal-frye@users.noreply.github.com>
2020-02-02 21:17:31 -0500

    Update qPCR Analysis.R

--- qPCR Analysis.R
+++ qPCR Analysis.R
@@ -1,16 +1,29 @@
-#Analysis of qPCR data
+# Analysis of qPCR data
 
-#Load packages and data
+#=============================================
+# Load packages and data
+#=============================================
+
 library(ggplot2)
 library(stringr)
 
-data <- read.csv("NAME", header = T)
+data <- read.delim(file.choose(), stringsAsFactors = F)
+
+plate.key <- read.table(file.choose(), header = T, stringsAsFactors = F)
+
+
+#=============================================
+# Understand & Refine data
+#=============================================
+Probe.fail <- as.factor(is.na(data$Cp))
+
+data.combined <- cbind(data[,3:6], plate.key$Probe, Probe.fail)
+
+data.combined$Pos <- as.factor(data.combined$Pos)
+data.combined$Name <- as.factor(data.combined$Name)
 
 
 
-
-#Need to load raw data
-#Clean the data? Investigate what samples failed to replicate
 #Indicate gene towhich all will be normalized
 #Do concentration calcuations w/ normalizing gene
 #Stastically analyse the data
